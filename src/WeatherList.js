7eaf1913d283cdc463ce0f097aaee1d9ef6133b9
gness1804 <graham.nessler@gmail.com>
2017-07-09 15:11:22 -0500

    Add key to turn off React error

--- src/WeatherList.js
+++ src/WeatherList.js
@@ -6,7 +6,7 @@ const WeatherList = ({ ...props }: Object) => {
   /* eslint-disable camelcase */
   const showWeatherData = (data: Object) => {
     const { title, icon_url, fcttext, pop } = data;
-    return (<div>
+    return (<div key={Math.random() * 1000}>
       <WeatherCard
         time={title}
         icon={icon_url}
