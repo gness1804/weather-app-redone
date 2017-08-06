7ad4ead12eb63c6d346ddca2003137e9b0bc2c13
gness1804 <graham.nessler@gmail.com>
2017-08-06 18:40:04 -0500

    Change header title to Fair Weather and remove dashes in title

--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,7 @@ import React from 'react';
 const Header = () => {
   return (
     <header className="box">
-      <h1>Welcome to -Weathrly-</h1>
+      <h1>Welcome to Fair Weather</h1>
       <h3>Your World<br />Your Weather</h3>
       <a className="wu-logo-link-wrapper" href="https://www.wunderground.com/" target="_blank" rel="noopener noreferrer">
         <figure className="wu-logo-container">
