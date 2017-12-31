a48b71746cb92e02e4ad6a4450c080d01b976b73
gness1804 <graham.nessler@gmail.com>
2017-12-31 15:40:17 -0600

    Add .idea to gitignore and create alldeploy script

---
+++ scripts/alldeploy.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+if test "$(git rev-parse --abbrev-ref HEAD)" != gh-pages; then
+  echo Oops, you must be on the gh-pages branch to deploy. >&2
+  exit 1
+fi
+
+git merge master
+npm run build
+npm run deploy
