ae5997f25a232351a710e92020ab700947bdae55
gness1804 <graham.nessler@gmail.com>
2018-01-01 20:26:39 -0600

    Create cities array in separate file

---
+++ src/helpers/cities.js
@@ -0,0 +1,21 @@
+// @flow
+
+const cities = [
+  'Atlanta',
+  'Austin',
+  'Blacksburg',
+  'Boston',
+  'Bryan',
+  'Chicago',
+  'Clemson',
+  'Denver',
+  'Detroit',
+  'Houston',
+  'Los Angeles',
+  'New Orleans',
+  'North Billerica',
+  'San Francisco',
+  'St Louis',
+];
+
+export default cities;
