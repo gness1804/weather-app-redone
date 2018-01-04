b95589ef8e6b435fb6f3ced1095f61104b157d0d
gness1804 <graham.nessler@gmail.com>
2018-01-03 19:25:22 -0600

    Change city strings to be objects with cities and states

--- src/helpers/cities.js
+++ src/helpers/cities.js
@@ -1,21 +1,66 @@
 // @flow
 
 const cities = [
-  'Atlanta',
-  'Austin',
-  'Blacksburg',
-  'Boston',
-  'Bryan',
-  'Chicago',
-  'Clemson',
-  'Denver',
-  'Detroit',
-  'Houston',
-  'Los Angeles',
-  'New Orleans',
-  'North Billerica',
-  'San Francisco',
-  'St Louis',
+  {
+    city: 'Atlanta',
+    state: 'GA',
+  },
+  {
+    city: 'Austin',
+    state: 'TX',
+  },
+  {
+    city: 'Blacksburg',
+    state: 'VA',
+  },
+  {
+    city: 'Boston',
+    state: 'MA',
+  },
+  {
+    city: 'Bryan',
+    state: 'TX',
+  },
+  {
+    city: 'Chicago',
+    state: 'IL',
+  },
+  {
+    city: 'Clemson',
+    state: 'SC',
+  },
+  {
+    city: 'Denver',
+    state: 'CO',
+  },
+  {
+    city: 'Detroit',
+    state: 'MI',
+  },
+  {
+    city: 'Houston',
+    state: 'TX',
+  },
+  {
+    city: 'Los Angeles',
+    state: 'CA',
+  },
+  {
+    city: 'New Orleans',
+    state: 'LA',
+  },
+  {
+    city: 'North Billerica',
+    state: 'MA',
+  },
+  {
+    city: 'San Francisco',
+    state: 'CA',
+  },
+  {
+    city: 'St Louis',
+    state: 'MO',
+  },
 ];
 
 export default cities;
